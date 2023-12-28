2a08d27ef6f98bedb5759bb60d85b2baf90c07c1
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2023-12-28 14:25:30 +0100

    Added database tables and Migrations. End implement Database section.

---
+++ LeaveManagementNet8.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementNet8.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
